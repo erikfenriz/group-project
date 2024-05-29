c583fc6e94ea27d1572d403760ca4fedaaf9888a
Martin <moragamartin2@gmail.com>
2024-05-29 12:11:01 -0300

    updating items routes and controllers

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -9,41 +9,87 @@ const getAll = async (req, res) => {
     // const collections = await db.listCollections().toArray();
     // console.log('Collections:', collections);
 
-    const result = await mongodb.getDb().db().collection(collectionName).find();
-    result.toArray().then((lists) => {
-      if (lists.length === 0) {
+    const result = await mongodb.getDb().db().collection(collectionNameItems).find();
+    result.toArray().then((items) => {
+      if (items.length === 0) {
         res.status(404).json({ message: 'There are no registered items' });
         return;
       }
-      res.status(200).json(lists);
+      res.status(200).json(items);
     });
 
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
-
 
 const getSingle = async (req, res) => {
   try {
     if (!ObjectId.isValid(req.params.id)) {
-      res.status(400).json({ message: 'You must use a valid article ID to find one.' });
+      res.status(400).json({ message: 'You must use a valid item ID' });
     }
 
     const itemId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection(collectionName).find({ _id: itemId });
+    const result = await mongodb.getDb().db().collection(collectionNameItems).find({ _id: itemId });
 
-    result.toArray().then((lists) => {
-        if (lists.length === 0) {
+    result.toArray().then((items) => {
+        if (items.length === 0) {
           res.status(404).json({message: 'The item with that ID does not exist.'});
           return;
         }
-        res.status(200).json(lists[0]);
+        res.status(200).json(items[0]);
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
+const getItemRating = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json({ message: 'You must use a valid item ID' });
+    }
+
+    const itemId = req.params.id;
+    const result = await mongodb.getDb().db().collection(collectionNameRating).find({ id: itemId });
+
+    result.toArray().then((ratings) => {
+      if (ratings.length === 0) {
+        res.status(404).json({message: 'The item with that ID does not exist.'});
+        return;
+      }
+      res.status(200).json(ratings[0]);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+const postItemRating = async (req, res) => {
+  try {
+    const itemId = new ObjectId(req.params.id);
+    const rating = {
+      rating: req.body.rating,
+      itemId: itemId,
+    };
+
+    const response = await mongodb.getDb().db().collection(collectionNameRating).insertOne(rating);
+    if (response.acknowledged) {
+      const result = await mongodb.getDb().db().collection(collectionNameRating).find({ itemId: itemId });
+      result.toArray().then((ratings) => {
+        if (ratings.length === 0) {
+          res.status(404).json({message: 'The item with that ID does not exist.'});
+          return;
+        }
+        res.status(201).json(ratings);
+      });
+    } else {
+      res.status(500).json({message: 'Some error occurred while creating the article.'})
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
 
 
 const createItem = async (req, res) => {
@@ -125,7 +171,9 @@ const deleteArticle = async (req, res) => {
 module.exports = {
     getAll,
     getSingle,
+    getItemRating,
+    postItemRating,
     createItem,
-    updateArticle,
-    deleteArticle
+    // updateArticle,
+    // deleteArticle
 };
--- server/routes/items.js
+++ server/routes/items.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const itemsController = require('../controllers/items');
-const { isAuthenticated } = require('../middleware/authenticate');
-const {validations, validate} = require("../middleware/validator");
+// const { isAuthenticated } = require('../middleware/authenticate');
+// const {validations, validate} = require("../middleware/validator");
 
 
 router.get('/', itemsController.getAll);
 router.get('/:id', itemsController.getSingle);
-router.post('/', validate, itemsController.createItem);
-router.put('/:id', isAuthenticated, validations().articles, validate, itemsController.updateArticle)
-router.delete('/:id', isAuthenticated, itemsController.deleteArticle)
+router.get('/', itemsController.getAll);
+router.get('/:id', itemsController.getSingle);
+router.get('/:id/rating', itemsController.getItemRating);
+router.post('/:id/rating', itemsController.postItemRating);
+router.post('/', itemsController.createItem);
 
 module.exports = router;
