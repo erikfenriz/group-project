4344a5e991161874a60be4de8dc7b6de454b06a6
erikfenriz <erikfenriz@gmail.com>
2024-06-01 14:20:25 +0200

    server clean up

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -3,6 +3,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 const collectionNameItems = 'items';
 const collectionNameRating = 'rating';
+const collectionNameComments = 'comments';
 
 const getAll = async (req, res) => {
   try {
@@ -80,6 +81,48 @@ const postItemRating = async (req, res) => {
         res.status(201).json(ratings);
       });
     } else {
+      res.status(500).json({message: 'Some error occurred while posting the rating.'})
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+const getItemComment = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json({ message: 'You must use a valid item ID' });
+    }
+
+    const itemId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection(collectionNameComments).find({ itemId: itemId });
+    result.toArray().then((ratings) => {
+      res.status(200).json(ratings);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+const postItemComment = async (req, res) => {
+  try {
+    const itemId = new ObjectId(req.params.id);
+    const comment = {
+      comment: req.body.comment,
+      itemId: itemId,
+    };
+
+    const response = await mongodb.getDb().db().collection(collectionNameComments).insertOne(comment);
+    if (response.acknowledged) {
+      const result = await mongodb.getDb().db().collection(collectionNameComments).find({ itemId: itemId });
+      result.toArray().then((comments) => {
+        if (comments.length === 0) {
+          res.status(404).json({message: 'The item with that ID does not exist.'});
+          return;
+        }
+        res.status(201).json(comments);
+      });
+    } else {
       res.status(500).json({message: 'Some error occurred while creating the item.'})
     }
   } catch (err) {
@@ -108,7 +151,7 @@ const createItem = async (req, res) => {
     if (response.acknowledged) {
       res.status(201).json(response);
     } else {
-      res.status(500).json ({message: 'Some error occurred while creating the item.'})
+      res.status(500).json ({message: 'Some error occurred while posting the comment.'})
     }
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -139,6 +182,8 @@ module.exports = {
     getSingle,
     getItemRating,
     postItemRating,
+    getItemComment,
+    postItemComment,
     createItem,
     deleteItem
 };
--- server/routes/items.js
+++ server/routes/items.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const itemsController = require('../controllers/items');
-// const { isAuthenticated } = require('../middleware/authenticate');
-// const {validations, validate} = require("../middleware/validator");
-
 
 router.get('/', itemsController.getAll);
 router.get('/:id', itemsController.getSingle);
@@ -12,9 +9,9 @@ router.get('/', itemsController.getAll);
 router.get('/:id', itemsController.getSingle);
 router.get('/:id/rating', itemsController.getItemRating);
 router.post('/:id/rating', itemsController.postItemRating);
+router.get('/:id/comments', itemsController.getItemComment);
+router.post('/:id/comments', itemsController.postItemComment);
 router.post('/', itemsController.createItem);
 router.delete('/:id', itemsController.deleteItem);
-// router.put('/:id', isAuthenticated, validations().articles, validate, itemsController.updateArticle)
-// router.delete('/:id', isAuthenticated, itemsController.deleteArticle)
 
 module.exports = router;
