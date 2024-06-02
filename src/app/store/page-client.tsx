c8521c22cc2d9501f3e3d65075e7d21a00a6dd80
erikfenriz <erikfenriz@gmail.com>
2024-06-02 17:31:33 +0200

    deployment errors_3

--- src/app/store/page-client.tsx
+++ src/app/store/page-client.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, {useEffect, useState} from 'react';
-// import {items} from '@/lib/placeholder-data';
 import styles from './styles.module.css';
 import ItemCard from "@/app/ui/item/item";
 import { Item } from "@/lib/definitions";
